8a2d7c994ad127ce6a1b0b6bbc17cf11da70625a
Sujin Minnich <sujinminnich@gmail.com>
2024-03-05 13:08:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# The Librarian
+
+## Description
+
+The librarian is the server for the book tracker 9000.
+
+## Technology Considerations
+
+#### Technology Used
+- JavaScript/TypeScript
+- [NodeJS](https://nodejs.org/en)
+- [NestJS](https://nestjs.com/)
+- [Prisma](https://www.prisma.io)
+- PostgresSQL
+
+## Running the project
+
+### Installation
+
+```bash
+$ npm install
+```
+
+### Running the app
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+### Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
+
+
---
+++ prisma/migrations/20240305172043_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "googleBooksId" INTEGER NOT NULL,
+    "coverUrl" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" SERIAL NOT NULL,
+    "firstName" VARCHAR(255) NOT NULL,
+    "lastName" VARCHAR(255) NOT NULL,
+    "googleBooksId" INTEGER NOT NULL,
+
+    CONSTRAINT "Author_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
